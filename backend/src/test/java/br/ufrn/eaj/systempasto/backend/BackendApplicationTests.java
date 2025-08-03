3caa4c2335faa931c29724d2c91e5e575453f22e
Vitor Paulino <vitorpaulinog@gmail.com>
2025-08-03 17:39:43 -0300

    first commit

---
+++ backend/src/test/java/br/ufrn/eaj/systempasto/backend/BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package br.ufrn.eaj.systempasto.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
